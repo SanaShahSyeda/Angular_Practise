4bbbb57fdc7d9d05ff9883ff7f84df8aa8ecf49a
Syeda Sana <sanashaaah4@gmail.com>
2024-01-24 23:46:45 +0500

    two-way binding between form's input and the property.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { NotesListComponent } from './notes-list/notes-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NotesListComponent],
+  imports: [RouterOutlet, HeaderComponent, NotesListComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angular_Practise';
+  name = 'John';
+
+  showName() {
+    alert(this.name);
+  }
 }
