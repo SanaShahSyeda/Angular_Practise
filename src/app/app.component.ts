3e547b4023b8b19898cbcfce19bebee14a98b734
Syeda Sana <sanashaaah4@gmail.com>
2024-01-24 23:16:53 +0500

    added notes-list component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NotesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angular_Practise';
-  logedIn = true;
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,35 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>{{ title }}</h1>
-    <p [class.red]="isRed">This is Some text</p>
-    <button (click)="change()">Click Me</button>
-    <!-- Using *ngFor directive to iterate over 'items' array -->
-    <div *ngFor="let item of items; trackBy: trackById">
-      {{ item.name }}
-    </div>
-  `,
+  template: '<h1>{{ title }}</h1>',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   title = 'My Notes';
-  isRed = false;
-  items: any[] = [
-    { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-    { id: 3, name: 'Item 3' },
-  ];
-
-  change() {
-    this.isRed = !this.isRed;
-  }
-
-  trackById(index: number, item: any): number {
-    return item.id;
-  }
 }
