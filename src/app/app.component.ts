fce9fb070d8f734f9d53cfb12b482b263b319741
Syeda Sana <sanashaaah4@gmail.com>
2024-01-24 12:36:46 +0500

    data binding using  @if and *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,5 @@ import { HeaderComponent } from './header/header.component';
 })
 export class AppComponent {
   title = 'Angular_Practise';
+  logedIn = true;
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
     <h1>{{ title }}</h1>
     <p [class.red]="isRed">This is Some text</p>
     <button (click)="change()">Click Me</button>
+    <!-- Using *ngFor directive to iterate over 'items' array -->
+    <div *ngFor="let item of items; trackBy: trackById">
+      {{ item.name }}
+    </div>
   `,
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   title = 'My Notes';
   isRed = false;
+  items: any[] = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+  ];
 
   change() {
     this.isRed = !this.isRed;
+  }
+
+  trackById(index: number, item: any): number {
+    return item.id;
   }
 }
