9aa7a0084c7896e74eda3077cc3dcdfab77e24a2
Syeda Sana <sanashaaah4@gmail.com>
2024-01-27 22:52:57 +0500

    implemented ReactiveForm using ReactievFormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
+
 import { NotesListComponent } from './notes-list/notes-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NotesListComponent, FormsModule],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    NotesListComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Angular_Practise';
-  name = 'John';
+  name = new FormControl('James');
 
   showName() {
-    alert(this.name);
+    alert(this.name.value);
   }
 }
